7d600d42a8f26b87b34d87dd88c271b6fc1281d0
smondal229 <smondal229@gmail.com>
2019-09-22 23:54:30 +0530

    data fetched and  loader added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Loader from './Loader';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async ()=>{
+      try
+      {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const json = await response.json();
+        setTodos(json);
+        setLoading(false);
+      }catch(e){setError(true)}
+    };
+    fetchData();
+  }, []);
+
+  const el = todos.map(todo=><li key={todo.id}>{todo.title}</li>);
+  
+  return(
+    <>
+      {
+        error?
+        <div>Something Went wrong</div>:
+        (loading?
+          <Loader/>:        
+          <ul>{el}</ul>)
+      }
+    </>);
 }
 
 export default App;
